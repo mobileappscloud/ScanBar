961d445e22089ee5690bb25858cd6804cc147be2
Faisal_Syed <doc92606@gmail.com>
2014-11-24 13:50:40 -0800

    Initial Commit

---
+++ ScanBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanBar
+//
+//  Created by Mac on 11/24/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
